0e6afc6ad9bfc9b8cabbc1f327eb1c8b3edebd25
Jumpei Ogawa <phanective@gmail.com>
2022-06-06 08:12:44 +0900

    fix: remove no-longer supported options in Mongoose 6
    see: https://mongoosejs.com/docs/migrating_to_6.html#no-more-deprecation-warning-options

--- index.js
+++ index.js
@@ -37,11 +37,7 @@ if (process.env.NODE_ENV === 'production') {
 
 (async function () {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to database');
   } catch (err) {
     throw err;
