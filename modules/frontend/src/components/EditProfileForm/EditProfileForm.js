5b3c5c05206fefe533665223f67f8e2482ff7b57
Jumpei Ogawa <phanective@gmail.com>
2022-09-03 16:07:20 +0900

    refactor: remove unused import

--- modules/frontend/src/components/EditProfileForm/EditProfileForm.js
+++ modules/frontend/src/components/EditProfileForm/EditProfileForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { useFormik } from 'formik';
 import { createStructuredSelector } from 'reselect';
