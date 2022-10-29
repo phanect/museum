b72f06735290ff4d07d38b0b5bfb1e3a187c2431
Jumpei Ogawa <phanective@gmail.com>
2022-10-29 16:31:20 +0900

    wip

--- modules/backend/index.js
+++ modules/backend/index.js
@@ -28,11 +28,11 @@ app.use('/api', apiRouter);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(compression());
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  //app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+  // app.get('*', function (req, res) {
+  //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  // });
 }
 
 (async function () {
